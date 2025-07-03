bc3b4127d4d8ac931dcc0d62af67ae7913b1194b
priyanshp2105 <133733698+priyanshp2105@users.noreply.github.com>
2025-07-03 22:45:31 +0530

    Update StringCalculator.java

--- string-calculator/src/main/java/com/incubyte/assignment/StringCalculator.java
+++ string-calculator/src/main/java/com/incubyte/assignment/StringCalculator.java
@@ -1,13 +1,16 @@
 package com.incubyte.assignment;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty())
             return 0;
 
-        String delimiter = ",|\n"; // Default delimiters
+        String delimiter = ",|\n";
 
         if (numbers.startsWith("//")) {
             int delimiterEndIndex = numbers.indexOf("\n");
@@ -16,10 +19,22 @@ public class StringCalculator {
         }
 
         String[] tokens = numbers.split(delimiter);
+        List<Integer> negatives = new ArrayList<>();
         int sum = 0;
+
         for (String token : tokens) {
-            sum += Integer.parseInt(token);
+            int num = Integer.parseInt(token);
+            if (num < 0) {
+                negatives.add(num);
+            }
+            sum += num;
         }
+
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException("negative numbers not allowed: " +
+                    negatives.stream().map(String::valueOf).collect(Collectors.joining(",")));
+        }
+
         return sum;
     }
 
