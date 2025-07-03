17b7cb677ce54221400235c245528ce8441cdab8
priyanshp2105 <133733698+priyanshp2105@users.noreply.github.com>
2025-07-03 22:18:13 +0530

    Update StringCalculator.java

--- string-calculator/src/main/java/com/incubyte/assignment/StringCalculator.java
+++ string-calculator/src/main/java/com/incubyte/assignment/StringCalculator.java
@@ -1,13 +1,21 @@
 package com.incubyte.assignment;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty())
             return 0;
 
-        // Replace newline with comma, then split
-        String[] tokens = numbers.replace("\n", ",").split(",");
+        String delimiter = ",|\n"; // Default delimiters
 
+        if (numbers.startsWith("//")) {
+            int delimiterEndIndex = numbers.indexOf("\n");
+            delimiter = Pattern.quote(numbers.substring(2, delimiterEndIndex));
+            numbers = numbers.substring(delimiterEndIndex + 1);
+        }
+
+        String[] tokens = numbers.split(delimiter);
         int sum = 0;
         for (String token : tokens) {
             sum += Integer.parseInt(token);
