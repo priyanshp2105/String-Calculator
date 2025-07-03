7837d6092c81460d8916057ea79a62daa826f566
priyanshp2105 <133733698+priyanshp2105@users.noreply.github.com>
2025-07-03 21:17:16 +0530

    Update StringCalculator.java

--- string-calculator/src/main/java/com/incubyte/assignment/StringCalculator.java
+++ string-calculator/src/main/java/com/incubyte/assignment/StringCalculator.java
@@ -2,6 +2,8 @@ package com.incubyte.assignment;
 
 public class StringCalculator {
     public int add(String numbers) {
-        return 0;
+        if (numbers.isEmpty())
+            return 0;
+        return Integer.parseInt(numbers);
     }
 }
