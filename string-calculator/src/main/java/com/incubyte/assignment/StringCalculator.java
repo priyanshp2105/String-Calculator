743668236eef8ae9a94e404a4a98122bf7efb98a
priyanshp2105 <133733698+priyanshp2105@users.noreply.github.com>
2025-07-03 21:31:37 +0530

    Update StringCalculator.java

--- string-calculator/src/main/java/com/incubyte/assignment/StringCalculator.java
+++ string-calculator/src/main/java/com/incubyte/assignment/StringCalculator.java
@@ -4,6 +4,17 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty())
             return 0;
+
+        if (numbers.contains(",")) {
+            String[] parts = numbers.split(",");
+            int sum = 0;
+            for (String part : parts) {
+                sum += Integer.parseInt(part);
+            }
+            return sum;
+        }
+
         return Integer.parseInt(numbers);
     }
+
 }
